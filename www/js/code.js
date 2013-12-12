fb0474e6c9310d5a096e9c145487dc9a72e96757
devtime-net <timvellaheino@gmail.com>
2013-12-12 13:58:29 +1000

    Another attempt at disabling sound

--- www/js/code.js
+++ www/js/code.js
@@ -1,4 +1,14 @@
+var disableTouch = false;
 function playSound(animal){
+	if (disableTouch == true){
+		return;
+	};
 	var media = new Media("/android_asset/www/sounds/" + animal + ".wav", null, null);
+	var duration = media.getDuration();
+	setTimeout(enableTouch(), duration);
 	media.play();
+};
+
+function enableTouch() {
+	disableTouch = false;
 };
