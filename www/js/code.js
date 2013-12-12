76f2a38c65144c57a1e4567bfeeb46614b442ead
devtime-net <timvellaheino@gmail.com>
2013-12-12 14:02:07 +1000

    Bugfix for disabling sound

--- www/js/code.js
+++ www/js/code.js
@@ -3,6 +3,7 @@ function playSound(animal){
 	if (disableTouch == true){
 		return;
 	};
+	disableTouch = true;
 	var media = new Media("/android_asset/www/sounds/" + animal + ".wav", null, null);
 	var duration = media.getDuration();
 	setTimeout(enableTouch(), duration);
