a7ff3cdc1d204b57cd6ff4b48ed67bcce2a121b1
devtime-net <timvellaheino@gmail.com>
2013-12-12 13:25:46 +1000

    More Animals

--- www/js/code.js
+++ www/js/code.js
@@ -1,13 +1,4 @@
 function playSound(animal){
-    var media = null;
-	if (animal == "cow") {
-		media = new Media("/android_asset/www/cow.wav",null,null);
-	} else if(animal == "sheep"){
-		media = new Media("/android_asset/www/sheep.wav",null,null);
-	}
-	
-	if (media != null){
-		media.play();
-	  //document.getElementById("Sound").innerHTML = "<embed src=\"" + soundFile + "\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
-	}
+    var media = new Media("/android_asset/www/sounds/" + animal + ".wav",null,null);	
+	media.play();
 };
