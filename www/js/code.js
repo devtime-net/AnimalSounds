e553ec42df5fcc912403bd82bcefc4297f7a21d6
devtime-net <timvellaheino@gmail.com>
2013-12-12 14:11:27 +1000

    Removing Sound Disable + splash screen fix

--- www/js/code.js
+++ www/js/code.js
@@ -1,15 +1,4 @@
-var disableTouch = false;
 function playSound(animal){
-	if (disableTouch == true){
-		return;
-	};
-	disableTouch = true;
 	var media = new Media("/android_asset/www/sounds/" + animal + ".wav", null, null);
-	var duration = media.getDuration();
-	setTimeout(enableTouch(), duration);
 	media.play();
-};
-
-function enableTouch() {
-	disableTouch = false;
 };
